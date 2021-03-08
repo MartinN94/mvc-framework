20e026e5b58234ad8223efea9764ad1ba0fe4c44
Martin Nikolov <nikolov.m@hotmail.com>
2021-03-08 13:00:46 +0100

    feat: DB connection and config added

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    //Database params
+    define('DB_HOST', 'localhost'); //Add your db host
+    define('DB_USER', 'root'); // Add your DB root
+    define('DB_PASS', 'secret'); //Add your DB pass
+    define('DB_NAME', 'mvcframework_db'); //Add your DB Name
+
+    //APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URLROOT (Dynamic links)
+    define('URLROOT', 'mvc-framework.test');
+
+    //Sitename
+    define('SITENAME', 'MVC Framework');
